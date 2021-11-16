06108ff3e3d7dd95359c786cea4f34e5b1488a2a
tirathawat@gmail.com <tirathawat@gmail.com>
2021-11-16 16:43:29 +0700

    add recover

--- server.go
+++ server.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/recover"
 	"github.com/joho/godotenv"
 )
 
@@ -35,7 +36,7 @@ func setupFiber() error {
 	app := fiber.New(getConfig())
 
 	app.Use(cors.New())
-	//app.Use(recover.New())
+	app.Use(recover.New())
 
 	router.New(app)
 	err := app.Listen(":" + os.Getenv("PORT"))
